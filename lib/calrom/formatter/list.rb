4c71c2e9601c0f74dea96a22e0611f8fcd6b05b7
Jakub Pavlík <severus@post.cz>
2019-10-05 13:25:12 +0200

    list: month headings

--- lib/calrom/formatter/list.rb
+++ lib/calrom/formatter/list.rb
@@ -2,10 +2,22 @@ module Calrom
   module Formatter
     class List < Formatter
       def call(days, date_range)
+        print_months = date_range.first.month != date_range.last.month
+
         puts date_range.to_s
         puts
 
+        current_month = nil
+
         days.each do |liturgical_day|
+          if print_months && liturgical_day.date.month != current_month
+            current_month = liturgical_day.date.month
+
+            puts
+            puts current_month
+            puts
+          end
+
           day liturgical_day
         end
       end
