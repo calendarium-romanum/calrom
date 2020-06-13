3252bcc91d6e84ff856800fdbb56b6ce1af640b5
Jakub Pavlik <severus@post.cz>
2020-06-13 11:21:19 +0200

    simplify

--- lib/calrom/formatter/overview.rb
+++ lib/calrom/formatter/overview.rb
@@ -11,7 +11,7 @@ module Calrom
           puts month
           puts
 
-          puts %w(Su Mo Tu We Th Fr Sa).join(' ')
+          puts 'Su Mo Tu We Th Fr Sa'
 
           print '   ' * month.first.wday
           month.each do |date|
