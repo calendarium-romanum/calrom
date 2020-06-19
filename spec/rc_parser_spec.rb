61d00fcdaddef533f19d6ce372394bf2a2165462
Jakub Pavlik <severus@post.cz>
2020-06-19 10:28:22 +0200

    more specs for configuration file parsing

--- spec/rc_parser_spec.rb
+++ spec/rc_parser_spec.rb
@@ -21,17 +21,17 @@ describe Calrom::RcParser do
       .to eq ['-o', 'quoted string']
   end
 
-  it 'multi-line' do
+  it 'handles newline as any other whitespace' do
     expect(described_class.("-a\n-b"))
       .to eq ['-a', '-b']
   end
 
-  it 'comment after a content' do
+  it 'comment after content' do
     expect(described_class.('-a # comment'))
       .to eq ['-a']
   end
 
-  it 'comment immediately after a content' do
+  it 'comment immediately after content' do
     expect(described_class.('-a# comment'))
       .to eq ['-a']
   end
@@ -39,5 +39,13 @@ describe Calrom::RcParser do
   it 'comment only' do
     expect(described_class.('# comment'))
       .to eq []
+  end
+
+  # tilde expansion to contents of $HOME is a frequently used shell feature
+  # and it may have sense to support it in the configuration files, but at least for now
+  # we are not going down the road of implementing "almost a real shell"
+  it 'does not expand tilde' do
+    expect(described_class.('~'))
+      .to eq ['~']
   end
 end
