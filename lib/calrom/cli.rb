626ee447bf7598631f9c7cd2c882668b1fc74c50
Jakub Pavlik <severus@post.cz>
2020-06-21 13:14:16 +0200

    handle all OptionParser exceptions

--- lib/calrom/cli.rb
+++ lib/calrom/cli.rb
@@ -10,7 +10,7 @@ module Calrom
         )
 
         calendar = config.calendar
-      rescue ::OptionParser::InvalidOption, InputError => e
+      rescue ::OptionParser::ParseError, InputError => e
         STDERR.puts e.message
         exit 1
       end
