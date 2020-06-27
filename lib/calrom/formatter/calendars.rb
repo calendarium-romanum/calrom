63a6540620b9a22148e55eee15dfba1c200c788f
Jakub Pavlik <severus@post.cz>
2020-06-27 21:22:42 +0200

    calendar listing: list parents

--- lib/calrom/formatter/calendars.rb
+++ lib/calrom/formatter/calendars.rb
@@ -5,10 +5,29 @@ module Calrom
       def call(calendar, date_range)
         last_locale = nil
         CR::Data.each do |d|
-          meta = d.load.metadata
+          sanctorale =
+            begin
+              d.load_with_parents
+            rescue StandardError => e
+              # As of calendarium-romanum 0.7.0, loading of one bundled data file fails like this
+              STDERR.puts "Parent loading for #{d.siglum.inspect} failed: #{e.message} (#{e.class})"
+              d.load
+            end
+          meta = sanctorale.metadata
           puts if last_locale && last_locale != meta['locale']
           default = d == Config::DEFAULT_DATA ? ' [default]' : ''
           puts "%-20s:  %s  [%s]%s" % [d.siglum, meta['title'], meta['locale'], default]
+
+          next unless meta['components']
+
+          parents =
+            meta['components']
+              .collect {|c| c['extends'] }
+              .compact
+              .map {|e| e.is_a?(Array) ? e : [e] } # 'extends' is String or Array
+              .flatten
+              .map {|p| p.sub(/\.\w{3,4}$/, '') } # file name to "siglum"
+          parents.each {|p| puts "  <  #{p}" }
           last_locale = meta['locale']
         end
       end
